c7901173f76a1c0634bcba91efbf144bfa574f1d
jiayuqiujq <jiayuqiujq>
2022-07-12 19:10:56 +0000

    Using Socrata library package to make GET request on API

--- Actuarial Accelerator.py
+++ Actuarial Accelerator.py
@@ -1,32 +1,58 @@
 # Databricks notebook source
 import pandas as pd
 import requests
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
 
 # COMMAND ----------
 
 # Reading in workers compensation data from csv file uploaded in local file store.
+spark_df_local_csv = spark.read.csv("/FileStore/tables/NY_workers_compensation.csv", header=True, inferSchema=True)
+display(spark_df_local_csv)
 
-# df = spark.read.csv("/FileStore/tables/NY_workers_compensation.csv", header=True, inferSchema=True)
+# COMMAND ----------
+
+# Checking number of records in spark df read from local csv
+spark_df_local_csv.count()
 
 # COMMAND ----------
 
 # Connecting to API using requests
-# r = requests.get('https://data.ny.gov/resource/jshw-gkgu.json')
-# workers_compensation = r.json()
+r = requests.get('https://data.ny.gov/resource/jshw-gkgu.json')
+r.status_code
 
 # COMMAND ----------
 
 # Reading csv directly from API using pandas
-df = pd.read_csv('https://data.ny.gov/resource/jshw-gkgu.csv')
+df = pd.DataFrame(workers_compensation)
+df
 
 # COMMAND ----------
 
-# Converting pandas df to spark df
-spark_df = spark.createDataFrame(df)
+len(df)
 
 # COMMAND ----------
 
-spark_df.collect()
+# Using sodapy to call API. Returns json - adjust timeout and limit to control number of records returned. Requires App Token from Socrata.
+from sodapy import Socrata
+
+client = Socrata("data.ny.gov",
+                  <insert App Token>, timeout=5000000)
+
+# First 2000 results, returned as JSON from API / converted to Python list of
+# dictionaries by sodapy.
+results = client.get("jshw-gkgu", limit=5000000)
+
+# Convert to pandas DataFrame
+spark_df = spark.createDataFrame(results)
+
+# COMMAND ----------
+
+display(spark_df)
+
+# COMMAND ----------
+
+spark_df.count()
 
 # COMMAND ----------
 
@@ -41,13 +67,5 @@ df_selected_cols = (df_selected_cols
                     .withColumn("Accident Date",to_date("Accident Date", "MM/dd/yyyy"))
                     .withColumn("Assembly date",to_date("Assembly Date", "MM/dd/yyyy"))
                     )
-
-
-
-# COMMAND ----------
-
-display(df_selected_cols)
-
-# COMMAND ----------
 
 
