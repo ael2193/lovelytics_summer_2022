f5e487010e8f780c89b33ba19bb615be7ccdcdf6
jiayuqiujq <jiayuqiujq>
2022-07-13 15:13:19 +0000

    Experimenting with Socrata SoQL functionality to query specific columns from API

--- Actuarial Accelerator.py
+++ Actuarial Accelerator.py
@@ -7,41 +7,29 @@ from pyspark.sql.functions import *
 # COMMAND ----------
 
 # Reading in workers compensation data from csv file uploaded in local file store.
-spark_df_local_csv = spark.read.csv("/FileStore/tables/NY_workers_compensation.csv", header=True, inferSchema=True)
-display(spark_df_local_csv)
-
-# COMMAND ----------
+# spark_df_local_csv = spark.read.csv("/FileStore/tables/NY_workers_compensation.csv", header=True, inferSchema=True)
 
 # Checking number of records in spark df read from local csv
-spark_df_local_csv.count()
+# spark_df_local_csv.count()
 
 # COMMAND ----------
 
 # Connecting to API using requests
-r = requests.get('https://data.ny.gov/resource/jshw-gkgu.json')
-r.status_code
+# r = requests.get('https://data.ny.gov/resource/jshw-gkgu.json')
+# r.status_code
 
 # COMMAND ----------
 
-# Reading csv directly from API using pandas
-df = pd.DataFrame(workers_compensation)
-df
-
-# COMMAND ----------
-
-len(df)
-
-# COMMAND ----------
-
-# Using sodapy to call API. Returns json - adjust timeout and limit to control number of records returned. Requires App Token from Socrata.
+# Using sodapy to call API. Returns json - adjust timeout and limit to control number of records returned.
 from sodapy import Socrata
 
-client = Socrata("data.ny.gov",
-                  <insert App Token>, timeout=5000000)
+client = Socrata("data.ny.gov", None, timeout=5000000)
 
-# First 2000 results, returned as JSON from API / converted to Python list of
-# dictionaries by sodapy.
-results = client.get("jshw-gkgu", limit=5000000)
+# Selected columns
+columns = 'accident_date', 'age_at_injury', 'average_weekly_wage', 'birth_year', 'carrier_name', 'claim_identifier', 'claim_injury_type', 'claim_type', 'closed_count', 'current_claim_status', 'gender', 'injured_in_county_name', 'wcio_cause_of_injury_code', 'wcio_cause_of_injury_desc', 'wcio_nature_of_injury_code', 'wcio_nature_of_injury_desc', 'wcio_pob_code', 'wcio_pob_desc', 'industry_code', 'industry_desc']
+
+# First 2000 results, returned as JSON from API / converted to Python list of dictionaries by sodapy.
+results = client.get("jshw-gkgu", limit=2000, select = columns)
 
 # Convert to pandas DataFrame
 spark_df = spark.createDataFrame(results)
@@ -53,6 +41,10 @@ display(spark_df)
 # COMMAND ----------
 
 spark_df.count()
+
+# COMMAND ----------
+
+spark_df.
 
 # COMMAND ----------
 
