5b146083c4d7d74a4f5350a54cb67c53c3115eb1
jiayuqiujq <jiayuqiujq>
2022-07-18 17:22:39 +0000

    Successfully converted date columns from StringType to DateType as part of Silver layer

--- Actuarial Accelerator.py
+++ Actuarial Accelerator.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
 from sodapy import Socrata
-from pyspark.sql.functions import *
+import pyspark.sql.functions as spark_functions
+import pyspark.sql.types as spark_type
 
 # COMMAND ----------
 
@@ -14,14 +15,13 @@ spark_df.count()
 
 # COMMAND ----------
 
+def rename_col(col):
+    return col.lower().replace(" ", "_").replace("description", "desc").replace("part_of_body", "pob")
+
+# COMMAND ----------
+
 for col in spark_df.columns:
-  spark_df = spark_df.withColumnRenamed(col, col.replace(" ", "_"))
-for col in spark_df.columns: 
-  spark_df = spark_df.withColumnRenamed(col, col.replace("Description", "desc"))
-for col in spark_df.columns:
-  spark_df = spark_df.withColumnRenamed(col, col.replace("Part_Of_Body", "pob"))
-for col in spark_df.columns:
-  spark_df = spark_df.withColumnRenamed(col, col.lower())
+  spark_df = spark_df.withColumnRenamed(col, rename_col(col))
 
 # COMMAND ----------
 
@@ -42,7 +42,7 @@ spark_df = (spark_df.withColumnRenamed('accident', 'accident_ind')
 # COMMAND ----------
 
 # Writing spark_df created from local csv file to Delta table
-spark_df.write.format('delta').mode('overwrite').option('overwriteSchema', True).option("path", '/user/jade.qiu@lovelytics.com/').saveAsTable("bronze_table")
+spark_df.write.format('delta').mode('overwrite').option("path", '/user/jade.qiu@lovelytics.com/').saveAsTable("bronze_table")
 
 # COMMAND ----------
 
@@ -101,7 +101,7 @@ spark_df_update.write.format('delta').mode('overwrite').option('overwriteSchema'
 
 # COMMAND ----------
 
-spark_df_silver = spark.read.format("delta").load('/user/jade.qiu@lovelytics.com/')
+spark_df_bronze = spark.read.format("delta").load('/user/jade.qiu@lovelytics.com/')
 
 # COMMAND ----------
 
@@ -113,14 +113,22 @@ display(spark_df_silver)
 
 # COMMAND ----------
 
-# Changing column schemas
+# Helper function to change date column schemas from string to date
+def string_to_date(date_string):
+  r1 = spark_functions.regexp_replace(date_string, "[T]", " ")
+  r2 = spark_functions.regexp_replace(r1, "[-]", "/")
+  r3 = spark_functions.to_date(r2, "MM/dd/yyyy")
+  return r3
 
-spark_df_silver = (spark_df_silver
-            .withColumn("accident_date", regexp_replace(spark_df['accident_date'],"[T]", " "))
-            .withColumn("accident_date",to_date("accident_date"))
-            .withColumn("ancr_date", regexp_replace(spark_df['ancr_date'],"[T]", " "))
-            .withColumn("ancr_date",to_date("ancr_date"))
-            )
+# COMMAND ----------
+
+# Change date column schemas from string to date
+
+date_columns = [col for col in spark_df_bronze.columns if 'date' in col]
+for col in date_columns:
+  spark_df_silver = spark_df_bronze.withColumn(col, string_to_date(col))
+  
+assert spark_df_silver.filter(spark_df_bronze.ancr_date.isNull()).count() == spark_df_silver.filter(spark_df_silver.ancr_date.isNull()).count()
 
 # COMMAND ----------
 
@@ -128,4 +136,59 @@ display(spark_df_silver)
 
 # COMMAND ----------
 
-display(spark.sql('SELECT * FROM bronze_table'))
+spark_df_silver = spark_df_silver.withColumn('days_btw_accident_and_claim_estd', 'ancr_date' - 'accident_date')
+
+# COMMAND ----------
+
+# Selecting more relevant columns for analysis
+selected_cols = ['accident_ind', 'accident_date', 'age_at_injury', 'ancr_date'
+assembly_date:date
+atty_rep_ind:string
+average_weekly_wage:double
+birth_year:integer
+c2_date:date
+c3_date:date
+carrier_name:string
+carrier_type:string
+claim_identifier:integer
+claim_injury_type:string
+claim_type:string
+closed_count:integer
+controverted_date:date
+injured_in_county_name:string
+covid_19_indicator:string
+current_claim_status:string
+district_name:string
+first_appeal_date:date
+first_hearing_date:date
+gender:string
+hearing_count:integer
+highest_process:string
+ime4_count:integer
+industry_code:integer
+industry_desc:string
+interval_assembled_to_ancr:integer
+medical_fee_region:string
+occupational_disease_ind:string
+oiics_event_exposure_code:string
+oiics_event_exposure_desc:string
+oiics_injury_source_code:string
+oiics_injury_source_desc:string
+oiics_nature_injury_code:string
+oiics_nature_injury_desc:string
+oiics_pob_code:string
+oiics_pob_desc:string
+oiics_secondary_source_code:string
+oiics_secondary_source_desc:string
+ppd_non_scheduled_loss_date:date
+ppd_scheduled_loss_date:date
+ptd_date:date
+section_32_date:date
+wcio_cause_of_injury_code:integer
+wcio_cause_of_injury_desc:string
+wcio_nature_of_injury_code:integer
+wcio_nature_of_injury_desc:string
+wcio_pob_code:integer
+wcio_pob_desc:string
+zip_code:string
+spark_df_silver = spark_df_silver.select()
